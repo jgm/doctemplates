17855e3d280eb2439ee63b53e83b27ad403b186e
John MacFarlane <jgm@berkeley.edu>
2019-08-23 13:06:37 -0700

    Removed unnecessary pragma.

--- src/Text/DocTemplates/Internal.hs
+++ src/Text/DocTemplates/Internal.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
